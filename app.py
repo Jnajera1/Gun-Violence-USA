1dfd87d36e4e7dabb0724976fecab86e15558d2a
Maria Quinton <mariaquinton@Marias-MBP.attlocal.net>
2021-01-23 13:28:41 -0500

    updated starter code

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify
+import data
+
+
+app = Flask(__name__, 
+            static_folder='static',
+            template_folder='templates')
+
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+
+@app.route('/db_data', methods=['GET'])
+def database_data():
+    # data = data.get_db_data()
+    data = {"this": "is my database data"}
+    return jsonify(data)
+
+
+@app.route('/api_data', methods=['GET'])
+def api_data():
+    # data = data.get_api_data()
+    data = {"this": "is my api data"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
