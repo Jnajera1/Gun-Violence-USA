478362646069c60dffc0106a3e8d96691e1e7385
Maria Quinton <mariaquinton@Marias-MacBook-Pro.local>
2021-01-29 14:34:55 -0500

    updates

--- static/js/map.js
+++ static/js/map.js
@@ -1,10 +1,10 @@
-// Creating map object
+// Create a map object
 var myMap = L.map("map", {
-  center: [40.7128, -74.0059],
-  zoom: 11
+  center: [37.09, -95.71],
+  zoom: 5
 });
 
-// Adding tile layer
+// Add a tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -14,11 +14,67 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Use this link to get the geojson data.
-var link = "datasets/combined.json";
+// An array containing each city's name, location, and population
+var cities = [{
+  coordinates: [31.1541, -94.3815],
+    state: {
+      name: "Shot - Dead (suicide) - Officer Involved Incident - Officer Involved Shooting - subject/suspect/perpetrator killed - Officer Involved Shooting - subject/suspect/perpetrator suicide at standoff - Domestic Violence",
+      date: '1/8/2018',
+      population: 29000000,
+      state: 'Texas' 
+    },
+},
+{
+  coordinates: [30.001, -95.4647],
+    state: {
+      name: "Shot - Wounded/Injured & Shot - Dead (suicide) - Officer Involved Incident - Officer Involved Shooting - Officer shot - Officer Involved Shooting - subject/suspect/perpetrator killed - Officer Involved Shooting - subject/suspect/perpetrator suicide at standoff - Officer Involved Shooting - Bystander shot - Police Targeted- Kidnapping/abductions/hostage||Possession of gun by felon or prohibited person",
+      date: '1/31/2018',
+      population: 29000000,
+      state: 'Texas' 
+    }
+},
+{
+  coordinates: [41.7146, -74.3921],
+    state: {
+      name: "Shot - Wounded/Injured & Shot - Dead (murder & suicide) - Murder/Suicide - Domestic Violence - LOCKDOWN/ALERT ONLY: No GV Incident Occurred Onsite",
+      date: '1/11/2018',
+      population: 29000000,
+      state: 'New York' 
+    }
+},
+{
+  coordinates: [29.5404, -98.639],
+    state: {
+      name: "Shot - Wounded/Injured||Shot - Dead (murder, accidental, suicide)||Suicide^||Murder/Suicide||Domestic Violence||Kidnapping/abductions/hostage",
+      date: '1/21/2018',
+      population: 29000000,
+      state: 'Texas' 
+    }
+},
+{
+  coordinates: [36.1808, -115.148],
+    state: {
+      name: "Shot - Wounded/Injured||Shot - Dead (murder, accidental, suicide)||Suicide^||Murder/Suicide||Domestic Violence",
+      date: '1/20/2018',
+      population: 29000000,
+      state: 'Nevada' 
+    }
+},
+{
+  coordinates: [36.1808, -115.148],
+    state: {
+      name: "Shot - Wounded/Injured||Shot - Dead (murder, accidental, suicide)||Suicide^||Murder/Suicide||Domestic Violence",
+      date: '1/20/2018',
+      population: 29000000,
+      state: 'Nevada' 
+    }
+},
+];
 
-// Grabbing our GeoJSON data..
-d3.json(link, function(data) {
-  // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
-});
+// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+for (var i = 0; i < cities.length; i++) {
+  var city = cities[i];
+  L.marker(city.coordinates)
+    .bindPopup("<h1>" + city.state.date + "</h1> <hr> <h3>Incident " + city.state.name + "</h3>")
+    .addTo(myMap);
+}
